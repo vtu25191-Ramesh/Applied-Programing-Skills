d9d888a48e3c39dcb60ab0bff53a45bb742ed270
vtu25191-Ramesh <vtu25191@veltech.edu.in>
2026-02-05 07:59:58 +0530

    Time: 3 ms (99.64%) | Memory: 94.7 MB (41.25%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // Step 1: Find middle of the list
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        // Step 3: Compare first and second halves
+        ListNode left = head;
+        ListNode right = prev;
+
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
