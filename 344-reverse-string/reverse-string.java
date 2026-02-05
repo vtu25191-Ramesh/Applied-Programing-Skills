a7581427982115d15cb2396a49717bef7440b95c
vtu25191-Ramesh <vtu25191@veltech.edu.in>
2026-02-05 07:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 48.7 MB (9.69%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            // swap characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    // Testing
+    public static void main(String[] args) {
+        char[] s1 = {'h','e','l','l','o'};
+        reverseString(s1);
+        System.out.println(Arrays.toString(s1));
+
+        char[] s2 = {'H','a','n','n','a','h'};
+        reverseString(s2);
+        System.out.println(Arrays.toString(s2));
+    }
+}
