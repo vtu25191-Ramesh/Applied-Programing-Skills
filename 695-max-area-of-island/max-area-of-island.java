7780c8464e9b1fa0cb4ee62d3a61050730dad225
vtu25191-Ramesh <vtu25191@veltech.edu.in>
2026-04-24 09:44:17 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (51.88%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,27 @@
+class Solution {
+        public int maxAreaOfIsland(int[][] grid) {
+                int max = 0;
+                        int rows = grid.length, cols = grid[0].length;
+
+                                for (int i = 0; i < rows; i++) {
+                                            for (int j = 0; j < cols; j++) {
+                                                            if (grid[i][j] == 1) {
+                                                                                max = Math.max(max, dfs(grid, i, j));
+                                                                                                }
+                                                                                                            }
+                                                                                                                    }
+                                                                                                                            return max;
+                                                                                                                                }
+
+                                                                                                                                    private int dfs(int[][] grid, int i, int j) {
+                                                                                                                                            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0)
+                                                                                                                                                        return 0;
+
+                                                                                                                                                                grid[i][j] = 0;
+
+                                                                                                                                                                        return 1 + dfs(grid, i+1, j)
+                                                                                                                                                                                         + dfs(grid, i-1, j)
+                                                                                                                                                                                                          + dfs(grid, i, j+1)
+                                                                                                                                                                                                                           + dfs(grid, i, j-1);
+                                                                                                                                                                                                                               }
+                                                                                                                                                                                                                               }
