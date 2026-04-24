ffb2fef294c1cf18d00ff6d14c285767a8935662
vtu25191-Ramesh <vtu25191@veltech.edu.in>
2026-04-24 09:40:34 +0530

    Time: 128 ms (55.84%) | Memory: 279.3 MB (73.12%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,26 @@
+class Solution {
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+                List<List<Integer>> graph = new ArrayList<>();
+                        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
+                                for (int[] e : edges) {
+                                            graph.get(e[0]).add(e[1]);
+                                                        graph.get(e[1]).add(e[0]);
+                                                                }
+
+                                                                        boolean[] visited = new boolean[n];
+                                                                                return dfs(graph, visited, source, destination);
+                                                                                    }
+
+                                                                                        private boolean dfs(List<List<Integer>> graph, boolean[] visited, int curr, int dest) {
+                                                                                                if (curr == dest) return true;
+
+                                                                                                        visited[curr] = true;
+                                                                                                                for (int nei : graph.get(curr)) {
+                                                                                                                            if (!visited[nei] && dfs(graph, visited, nei, dest)) {
+                                                                                                                                            return true;
+                                                                                                                                                        }
+                                                                                                                                                                }
+                                                                                                                                                                        return false;
+                                                                                                                                                                            }
+                                                                                                                                                                            }
