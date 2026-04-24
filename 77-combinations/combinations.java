6e908e32e8160355f570f0f7d21200f9156eea2a
vtu25191-Ramesh <vtu25191@veltech.edu.in>
2026-04-24 10:51:36 +0530

    Time: 18 ms (82.31%) | Memory: 97.9 MB (67.40%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), 1, n, k);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> temp, int start, int n, int k) {
+        if (temp.size() == k) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            temp.add(i);
+            backtrack(res, temp, i + 1, n, k);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
